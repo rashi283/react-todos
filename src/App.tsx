26c49de7accc286371986da144484e00ef45aa31
Rashi Agrawal <rashiaa@verizonmedia.com>
2023-07-11 20:43:36 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,91 @@
-import React, { useCallback, useState } from 'react'
-import { v4 as uuid } from 'uuid'
-import styled from '@emotion/styled'
-import type { Todo } from './types'
-import * as localStorageServices from './services/localStorage'
-import { sortTodos } from './models/sortTodos.models'
-import { AddInput } from './components/AddInput'
-import { TodoItem } from './components/TodoItem'
-import { TodoList } from './components/TodoList'
-import { Header } from './components/Header'
+import React, { useCallback, useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import styled from "@emotion/styled";
+import { AddInput } from "./components/AddInput";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { Header } from "./components/Header";
 
 const Wrapper = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
   width: 300,
-  marginBottom: '50px',
-})
+});
 
 /**
- * This is the initial todo state.
- * Instead of loading this data on every reload,
- * we should save the todo state to local storage,
- * and restore on page load. This will give us
- * persistent storage.
- */
-const initialData: Todo[] = localStorageServices.getTodos() || []
+* This is the initial todo state.
+* Instead of loading this data on every reload,
+* we should save the todo state to local storage,
+* and restore on page load. This will give us
+* persistent storage.
+*/
+const initialData: Todo[] = [
+  {
+    id: "1",//uuid(),
+    label: "Buy groceries",
+    checked: false,
+  },
+  {
+    id: "2",//uuid(),
+    label: "Reboot computer",
+    checked: false,
+  },
+  {
+    id: "3",//uuid(),
+    label: "Ace CoderPad interview",
+    checked: true,
+  },
+];
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>(initialData)
+  const [todos, setTodos] = useState<Todo[]>(initialData);
 
   const addTodo = useCallback((label: string) => {
-    setTodos(prev => [
+    setTodos((prev) => [
       {
         id: uuid(),
         label,
         checked: false,
-        createdAt: new Date(),
-        completedAt: null,
       },
       ...prev,
     ])
-    localStorageServices.setTodos(todos)
+    window.localStorage.setItem('todoKey', JSON.stringify(todos))
+    console.log(window.localStorage.getItem('todoKey'))
+  }, []);
+  useEffect(()=>{
+    const num = Math.floor(Math.random()*200)
+    fetch('https://jsonplaceholder.typicode.com/todos/'+num)
+    .then(res=>res.json())
+    .then(res=>{
+      setTodos(todos=>[...todos, {
+        id: uuid(),
+        label:res.title,
+        checked: res.completed,
+      }])
+    })
   }, [])
 
-  const handleChange = useCallback(
-    ({ id, checked }: { id: string; checked: boolean }) => {
-      const todoIndex = todos.findIndex(todo => todo.id === id)
-      if (todoIndex === -1) {
-        console.warn('todo not found. id:', id)
-        return
-      }
-      const newTodos = [...todos]
-      newTodos[todoIndex].checked = checked
-      newTodos[todoIndex].completedAt = checked ? new Date() : null
-      const sortedNewTodos = sortTodos(newTodos)
-      setTodos(sortedNewTodos)
-      localStorageServices.setTodos(sortedNewTodos)
-    },
-    [todos]
-  )
-
-  const handleRemove = useCallback(
-    ({ id }: { id: string }) => {
-      const newTodos = todos.filter(todo => todo.id !== id)
-      setTodos(newTodos)
-      localStorageServices.setTodos(newTodos)
-    },
-    [todos]
-  )
+  const handleChange = useCallback(({id, checked}: {id: string, checked: boolean}) => {
+    // handle the check/uncheck logic
+    const todoIndex = todos.findIndex(todo => todo.id === id)
+    const newTodos = [...todos]
+    newTodos[todoIndex].checked = checked
+    setTodos(newTodos)
+    
+  }, [todos]);
 
   return (
     <Wrapper>
       <Header>Todo List</Header>
       <AddInput onAdd={addTodo} />
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} onChange={handleChange} onRemove={handleRemove} />
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onChange={handleChange} />
         ))}
       </TodoList>
     </Wrapper>
-  )
+  );
 }
 
-export default App
+export default App;
